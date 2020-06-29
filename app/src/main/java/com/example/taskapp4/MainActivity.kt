48bac54513c2008336b4e85b055614063d0f2856
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-06-29 23:01:47 +0900

    4

--- app/src/main/java/com/example/taskapp4/MainActivity.kt
+++ app/src/main/java/com/example/taskapp4/MainActivity.kt
@@ -3,12 +3,11 @@ package com.example.taskapp4
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mTaskAdapter:TaskAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,21 +17,30 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        //ListViewの設定
+        mTaskAdapter = TaskAdapter(this@MainActivity)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        //Listviewをタップした時の処理
+        listView1.setOnItemClickListener { parent, view, position, id ->
+            //入力・編集する画面に遷移させる)
         }
+
+        //ListViewを長押ししたときの処理
+        listView1.setOnItemLongClickListener{parent, view, position, id ->
+            //タスクを削除する
+            true
+        }
+
+        reloadListView()
+    }
+
+   private fun reloadListView() {
+       //後でTaskクラスに変更する
+       val taskList = mutableListOf("aaa", "bbb", "ccc")
+
+       mTaskAdapter.taskList = taskList
+       listView1.adapter = mTaskAdapter
+       mTaskAdapter.notifyDataSetChanged()
     }
 }
